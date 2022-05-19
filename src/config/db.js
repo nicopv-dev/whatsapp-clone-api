8e50bff590893d43f1f3cc121bc96afa3830d5f1
Nicolas Pereira <nicopv.dev@gmail.com>
2022-05-18 23:01:19 -0400

    nuevo commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
-import { MONGO_USER, MONGO_PASS } from "../utils/constants.js";
-
-const URL_CONNECTION = "mongodb://localhost:27017/whatsapp-clone";
+import { MONGO_USER, MONGO_PASS, MONGO_URI } from "../utils/constants.js";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(URL_CONNECTION);
+    await mongoose.connect(MONGO_URI);
     console.log("Connected to MongoDB..");
   } catch (error) {
     console.log(error);
