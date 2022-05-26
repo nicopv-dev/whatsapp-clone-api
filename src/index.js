d60dd5783509eafb00fddb805765a3c9d6a91d09
Nicolas Pereira <nicopv.dev@gmail.com>
2022-05-25 23:23:27 -0400

    agregado cors manualmente v5

--- src/index.js
+++ src/index.js
@@ -42,7 +42,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader(
